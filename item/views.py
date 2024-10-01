da88cc471c01a7456a101173c71e3706a2226ea1
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-01 01:11:24 +0100

    add delete and edit operations done

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 def detail(request, pk):
@@ -29,3 +29,27 @@ def new(request):
         'form': form,
         'title': 'New Item',
     })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
--- online_marketplace_with_django/settings.py
+++ online_marketplace_with_django/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     #apps
     'core',
     'item',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
